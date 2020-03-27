4048d975fc9ea1679b9708d74ab9f4a9ba678f1c
Shrishti-Saraswat <60365385+Shrishti-Saraswat@users.noreply.github.com>
2020-03-27 13:38:48 +0530

    Add files via upload

---
+++ dice stimulator.py
@@ -0,0 +1,49 @@
+#This is the code for a dice stimulator in which a dice is rolled multple times as per the wishes of the users.
+import random
+print("\nThis is a DICE STIMULATOR\n")
+x = "y"
+
+while x == "y":
+
+    #to generate a random no(1,6) to print the face of the dice.
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    O   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+    x = input("Press y to roll again.........\n")
+else:
+    print("Game ends..............")
